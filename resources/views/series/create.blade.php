e389e8d3dd3c9c879a0d5ede2254dbf9a7968d14
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-13 12:53:52 -0300

    feat: add bootstrap

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,9 @@
+<x-layout title="Nova Série">
+    <form action="" method="post">
+        <div class="mb-3">
+            <label for="name" class="form-label">Nome:</label>
+            <input type="text" name="name" value="" id="name" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Adicionar</button>
+    </form>
+</x-layout>
