030764904a7185fb2d15e545a7e599e32333ac94
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-13 12:49:45 -0300

    feat: add series controller

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request)
+    {
+        $series = ["Punisher", "Lost", "Grey"];
+        return view("series.index")->with("series", $series);
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+}
