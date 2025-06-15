3e3357becbcf0dcd3ecac20cdeab0b87d91d2701
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-15 16:52:27 -0300

    feat: add form action salvar

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,5 +1,5 @@
 <x-layout title="Nova Série">
-    <form action="" method="post">
+    <form action="/series/salvar" method="post">
         <div class="mb-3">
             <label for="name" class="form-label">Nome:</label>
             <input type="text" name="name" value="" id="name" class="form-control">
