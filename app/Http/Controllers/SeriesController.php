f8aca8cff72f5f26561799a0ebe89b2697bac00e
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-17 11:25:34 -0300

    feat: add return function

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,17 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
+use App\Mail\SeriesCreated;
 use App\Models\Series;
+use App\Models\User;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Repositories\SeriesRepository;
-use App\Mail\SeriesCreated;
 use Illuminate\Support\Facades\Mail;
 
 class SeriesController extends Controller
 {
-    public function __construct(private SeriesRepository $repository) {}
+    public function __construct(
+        private SeriesRepository $repository
+    ) {
+    }
 
-    public function index(Request $request)
+    public function index(Request $request): View
     {
         $series = Series::all();
         $messageSuccess = $request->session()->get("message.success");
@@ -22,28 +29,31 @@ class SeriesController extends Controller
             ->with("messageSuccess", $messageSuccess);
     }
 
-    public function create()
+    public function create(): View|Factory
     {
         return view("series.create");
     }
 
-    public function store(SeriesFormRequest $request)
+    public function store(SeriesFormRequest $request): RedirectResponse
     {
         $serie = $this->repository->add($request);
-        $email = new SeriesCreated(
-            $serie->name,
-            $serie->id,
-            $request->seasons,
-            $request->episodesPerSeason
-        );
-        Mail::to($request->user())->send($email);
+        $userList = User::all();
+        foreach ($userList as $user) {
+            $email = new SeriesCreated(
+                $serie->name,
+                $serie->id,
+                $request->seasons,
+                $request->episodesPerSeason
+            );
+            Mail::to($user)->send($email);
+        }
         return to_route("series.index")->with(
             "message.success",
             "Série '{$serie->name}' criada com sucesso"
         );
     }
 
-    public function destroy(Series $series, Request $request)
+    public function destroy(Series $series, Request $request): RedirectResponse
     {
         $series->delete();
         return to_route("series.index")->with(
@@ -52,12 +62,12 @@ class SeriesController extends Controller
         );
     }
 
-    public function edit(Series $series)
+    public function edit(Series $series): View
     {
         return view("series.edit")->with("serie", $series);
     }
 
-    public function update(Series $series, SeriesFormRequest $request)
+    public function update(Series $series, SeriesFormRequest $request): RedirectResponse
     {
         $series->fill($request->all());
         $series->save();
