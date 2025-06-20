498974b06d02cb06f29d2701e6dc0ac39112bc49
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-20 14:13:01 -0300

    test: add series controller list all series

--- tests/Feature/Http/Controllers/Api/SeriesControllerTest.php
+++ tests/Feature/Http/Controllers/Api/SeriesControllerTest.php
@@ -1,31 +1,31 @@
 <?php
 
+namespace Tests\Feature\Http\Controllers\Api;
+
 use App\Models\Episode;
 use App\Models\Season;
 use App\Models\Series;
-use Carbon\Carbon;
+use function Pest\Laravel\get;
 
-test(
-    'example',
-    function () {
-        $series = Series::factory()
-            ->has(
-                Season::factory()->count(3)->has(
-                    Episode::factory()->count(3)
-                )
-            )->create();
-        $response = $this->get("/api/series/");
-        $data = [
-            [
-                'name' => $series->name,
-                'created_at' => $series->created_at->toIsoString(),
-                'updated_at' => $series->updated_at->toIsoString(),
-                'cover' => $series->cover
-            ]
-        ];
-        $response
-            ->assertStatus(200)
-            ->assertHeader('Content-Type', 'application/json')
-            ->assertJson($data);
-    }
-);
+test("list all series", function () {
+    $series = Series::factory()
+        ->has(
+            Season::factory()
+                ->count(3)
+                ->has(Episode::factory()->count(3))
+        )
+        ->create();
+    $data = [
+        [
+            "name" => $series->name,
+            "created_at" => $series->created_at->toIsoString(),
+            "updated_at" => $series->updated_at->toIsoString(),
+            "cover" => $series->cover,
+        ],
+    ];
+    get("/api/series/")
+        ->assertStatus(200)
+        ->assertHeader("Content-Type", "application/json")
+        ->assertJson($data)
+        ->assertJsonCount(1);
+});
