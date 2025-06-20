4393e4466ab09328a71d59738f76d4a5c9586849
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-20 14:10:35 -0300

    refactor: add functions return

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -5,19 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Episode extends Model
 {
     use HasFactory;
     protected $fillable = ["number"];
-    protected $casts = ["watched" => "boolean"];
+    protected $casts = [
+        "watched" => "boolean",
+    ];
 
-    public function season()
+    public function season(): BelongsTo
     {
         return $this->belongsTo(Season::class);
     }
 
-    public function scopeWatched(Builder $query)
+    public function scopeWatched(Builder $query): void
     {
         $query->where("watched", true);
     }
