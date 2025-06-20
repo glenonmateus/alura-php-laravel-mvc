a28635488fb11aec3c253637af404b5ad91d0dbf
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-20 14:11:58 -0300

    refactor: remove unused imports

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\EpisodesController;
+use App\Http\Controllers\Api\SeasonsController;
 use App\Http\Controllers\Api\SeriesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,11 +17,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource(
-    '/series',
-    SeriesController::class
-);
+Route::apiResource("/series", SeriesController::class);
+
+Route::controller(SeasonsController::class)->group(function () {
+    Route::get("/series/{series}/seasons", "index");
+});
+
+Route::controller(EpisodesController::class)->group(function () {
+    Route::get("/series/{series}/episodes", "index");
+    Route::patch("/episodes/{episode}", "update");
+});
