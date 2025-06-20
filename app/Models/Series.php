41493d1efd64f86b6bbab8b16c6697828b530810
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-20 14:10:50 -0300

    refactor: add functions return

--- app/Models/Series.php
+++ app/Models/Series.php
@@ -5,18 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Series extends Model
 {
     use HasFactory;
     protected $fillable = ["name", "cover"];
 
-    public function seasons()
+    public function seasons(): HasMany
     {
         return $this->hasMany(Season::class, 'series_id');
     }
 
-    public static function booted()
+    public function episodes(): HasManyThrough
+    {
+        return $this->hasManyThrough(Episode::class, Season::class);
+    }
+
+    public static function booted(): void
     {
         self::addGlobalScope(
             'ordered',
