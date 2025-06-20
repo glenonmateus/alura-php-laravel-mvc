f9841404733553b69ff012bb8da450a8ec934dae
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-20 15:23:17 -0300

    test: use expectations

--- tests/Feature/SeriesRepositoryTest.php
+++ tests/Feature/SeriesRepositoryTest.php
@@ -5,33 +5,17 @@ use App\Models\Season;
 use App\Models\Series;
 
 test(
-    'test when a serie is created its seasons and episodes must also be created',
+    "test when a serie is created its seasons and episodes must also be created",
     function () {
         $series = Series::factory()
             ->has(
-                Season::factory()->count(3)->has(
-                    Episode::factory()->count(3)
-                )
-            )->create();
-        $this->assertDatabaseHas(
-            'series',
-            ['id' => $series->id]
-        );
-        $this->assertDatabaseHas(
-            'seasons',
-            ['series_id' => $series->id]
-        );
-        $this->assertDatabaseHas(
-            'episodes',
-            ['season_id' => $series->seasons->first()->id]
-        );
-        $this->assertEquals(
-            3,
-            Season::where('series_id', $series->id)->count()
-        );
-        $this->assertEquals(
-            3,
-            Episode::where('season_id', $series->seasons->first()->id)->count()
-        );
+                Season::factory()
+                    ->count(3)
+                    ->has(Episode::factory()->count(3))
+            )
+            ->create();
+        expect($series)->toBeInstanceOf(Series::class);
+        expect($series->seasons)->toHaveCount(3);
+        expect($series->episodes)->toHaveCount(9);
     }
 );
