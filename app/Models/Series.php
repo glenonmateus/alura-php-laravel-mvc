b6b708bd805d7f5d5a7ce4d44523e5c55d9b65ba
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-23 11:06:56 -0300

    feat: add model attribute links

--- app/Models/Series.php
+++ app/Models/Series.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Attribute as AttributeAttribute;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -12,10 +14,11 @@ class Series extends Model
 {
     use HasFactory;
     protected $fillable = ["name", "cover"];
+    protected $appends = ["links"];
 
     public function seasons(): HasMany
     {
-        return $this->hasMany(Season::class, 'series_id');
+        return $this->hasMany(Season::class, "series_id");
     }
 
     public function episodes(): HasManyThrough
@@ -25,11 +28,28 @@ class Series extends Model
 
     public static function booted(): void
     {
-        self::addGlobalScope(
-            'ordered',
-            function (Builder $builder) {
-                $builder->orderBy('name', 'asc');
-            }
+        self::addGlobalScope("ordered", function (Builder $builder) {
+            $builder->orderBy("name", "asc");
+        });
+    }
+
+    public function links(): Attribute
+    {
+        return new Attribute(
+            get: fn() => [
+                [
+                    "rel" => "self",
+                    "url" => route("series.show", $this),
+                ],
+                [
+                    "rel" => "seasons",
+                    "url" => route("seasons.index", $this),
+                ],
+                [
+                    "rel" => "episodes",
+                    "url" => route("episodes.index", $this),
+                ],
+            ]
         );
     }
 }
