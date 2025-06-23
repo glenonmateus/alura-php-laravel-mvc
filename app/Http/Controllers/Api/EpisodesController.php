583a73f5a3272ec404042dc12ec8f3037fe13d80
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-23 11:08:08 -0300

    feat: add episodes api controller

---
+++ app/Http/Controllers/Api/EpisodesController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Episode;
+use App\Models\Series;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class EpisodesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(int $id): JsonResponse
+    {
+        $series = Series::findOrFail($id);
+
+        return response()->json($series->episodes);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request): void
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id): void
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Episode $episode, SeriesFormRequest $request): JsonResponse
+    {
+        $episode->watched = $request->watched;
+        $episode->save();
+        return response()->json($episode);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id): void
+    {
+        //
+    }
+}
