697913e35b4168ab763b44f37ba7971934771b6e
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-23 11:16:06 -0300

    refactor: remove unused

--- app/Models/Series.php
+++ app/Models/Series.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Attribute as AttributeAttribute;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
