43d218a65490334e43a2301a5a9e7485e816a5fb
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-23 11:05:55 -0300

    refactor: add function returns

--- app/Models/Season.php
+++ app/Models/Season.php
@@ -4,26 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Season extends Model
 {
     use HasFactory;
     protected $fillable = ["number"];
 
-    public function series()
+    public function series(): BelongsTo
     {
         return $this->belongsTo(Series::class);
     }
 
-    public function episodes()
+    public function episodes(): HasMany
     {
         return $this->hasMany(Episode::class);
     }
 
-    public function numberOfWatchedEpisodes()
+    public function numberOfWatchedEpisodes(): int
     {
         return $this->episodes
-            ->filter(fn($episode) => $episode->watched)
+            ->filter(fn ($episode) => $episode->watched)
             ->count();
     }
 }
